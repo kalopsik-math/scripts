0345cb34d13fa98f8997107ef1a22fa5817cd220
Dimitris Kalopsikakis <kalopsik@gmail.com>
2015-03-01 00:46:58 +0200

    20150301004658

--- misc/mydate.py
+++ misc/mydate.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
-# This is a tiny script to give the current date and time
-# in a format that i can use it as part of a filename. For
-# example if i want to save an incoming letter i insert the
-# current date in this format in the filename.
+# Gives time in a format appropriate for being part of a file name.
 
 import time
 
-print time.strftime("%Y%m%d%H%M%S").rstrip('\n')
+print(time.strftime("%Y%m%d%H%M%S").rstrip('\n'))
