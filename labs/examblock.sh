38723a043b461d48d9af4e26c7f744d4be4e4796
Dimitris Kalopsikakis <kalopsik@gmail.com>
2015-03-01 17:14:43 +0200

    20150301171443

--- labs/examblock.sh
+++ labs/examblock.sh
@@ -4,15 +4,12 @@
 # By default, all open and clear.
 init()
 {
-    echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-
     iptables -t nat -F OUTPUT
     iptables -t nat -F PREROUTING
     iptables -t nat -F POSTROUTING
     iptables -t nat -X
-    iptables -t nat -P OUTPUT ACCEPT
-    iptables -t nat -P PREROUTING ACCEPT
-    iptables -t nat -P POSTROUTING ACCEPT
+
+    p_nat "ACCEPT"
 
     iptables -t mangle -F INPUT
     iptables -t mangle -F OUTPUT
@@ -20,63 +17,60 @@ init()
     iptables -t mangle -F PREROUTING
     iptables -t mangle -F POSTROUTING
     iptables -t mangle -X
-    iptables -t mangle -P INPUT ACCEPT
-    iptables -t mangle -P OUTPUT ACCEPT
-    iptables -t mangle -P FORWARD ACCEPT
-    iptables -t mangle -P PREROUTING ACCEPT
-    iptables -t mangle -P POSTROUTING ACCEPT
+
+    p_mangle "ACCEPT"
     
 
     iptables -t filter -F INPUT
     iptables -t filter -F OUTPUT
     iptables -t filter -F FORWARD
     iptables -t filter -X
-    iptables -t filter -P INPUT ACCEPT
-    iptables -t filter -P OUTPUT ACCEPT
-    iptables -t filter -P FORWARD ACCEPT
+    
+    p_filter "ACCEPT"
 }
 
 p_filter()
 {
-    echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAA"
     iptables -t filter -P INPUT $1
-    iptables -t filter -P OUTPUT DROP
-    iptables -t filter -P FORWARD DROP
+    iptables -t filter -P OUTPUT $1
+    iptables -t filter -P FORWARD $1
 }
 
 
-drop_nat()
+p_nat()
 {
-    iptables -t nat -P OUTPUT DROP
-    iptables -t nat -P PREROUTING DROP
-    iptables -t nat -P POSTROUTING DROP
+    iptables -t nat -P OUTPUT $1
+    iptables -t nat -P PREROUTING $1
+    iptables -t nat -P POSTROUTING $1
 }
 
-drop_mangle()
+p_mangle()
 {
-    iptables -t mangle -P INPUT DROP
-    iptables -t mangle -P OUTPUT DROP
-    iptables -t mangle -P FORWARD DROP
-    iptables -t mangle -P PREROUTING DROP
-    iptables -t mangle -P POSTROUTING DROP
+    iptables -t mangle -P INPUT $1
+    iptables -t mangle -P OUTPUT $1
+    iptables -t mangle -P FORWARD $1
+    iptables -t mangle -P PREROUTING $1
+    iptables -t mangle -P POSTROUTING $1
 }
 
 on()
 {
-    
-    
-    echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAA"
     init
 
-    p_filter "ACCEPT"
+    p_filter DROP
+    p_mangle DROP
+    p_nat    DROP
 
     # Allow all traffic with localhost
     iptables -A INPUT  -i lo -j ACCEPT
     iptables -A OUTPUT -o lo -j ACCEPT
 
 
+    # Allow outbound http/https to fourier.math.uoc.gr
     iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-    iptables -A OUTPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
+    iptables -A OUTPUT -m state --state NEW,ESTABLISED,RELATED -p tcp -d 147.52.65.68 --dport 80,443 -j ACCEPT
+    iptables -A INPUT  -m state --state ESTABLISED,RELATED -p tcp -s 147.52.65.68 --sport 80,443 -j ACCEPT
+
     iptables -A OUTPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
 
     #ssh:
@@ -90,18 +84,18 @@ on()
 
 
     # Allow outbound DNS
-    iptables -A OUTPUT -m state --state NEW, ESTABLISHED -p udp --dport 53 -j ACCEPT
+    iptables -A OUTPUT -m state --state NEW,ESTABLISHED -p udp --dport 53 -j ACCEPT
     iptables -A INPUT  -m state --state ESTABLISHED      -p udp --sport 53 -j ACCEPT
-    iptables -A OUTPUT -m state --state NEW, ESTABLISHED -p tcp --dport 53 -j ACCEPT
+    iptables -A OUTPUT -m state --state NEW,ESTABLISHED -p tcp --dport 53 -j ACCEPT
     iptables -A INPUT  -m state --state ESTABLISHED      -p tcp --sport 53 -j ACCEPT
 
 }
 
 off()
 {
-    iptables -P INPUT ACCEPT
-    iptables -P OUTPUT ACCEPT
-    iptables -P FORWARD ACCEPT
+    p_filter "ACCEPT"
+    p_nat "ACCEPT"
+    p_mangle "ACCEPT"
 
     iptables -F INPUT
     iptables -F OUTPUT
