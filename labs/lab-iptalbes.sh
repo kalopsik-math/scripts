2807964c78099a93e1f0fef7f17d4aadd27c603d
Dimitris Kalopsikakis <kalopsik@gmail.com>
2015-03-01 21:42:34 +0200

    20150301214234

--- labs/lab-iptalbes.sh
+++ labs/lab-iptalbes.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
-iptables -F INPUT
-iptables -F OUTPUT
-iptables -F FORWARD
+RETVAL=1
 
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+case $1 in
 
-iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-iptables -A INPUT -p udp --sport 53 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
-iptables -A INPUT -p tcp --sport 53 -j ACCEPT
+    on)
+        iptables -P INPUT DROP
+        iptables -P OUTPUT DROP
+        iptables -P FORWARD DROP
+        
+        iptables -F INPUT
+        iptables -F OUTPUT
+        iptables -F FORWARD
+    
+        iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+        iptables -A INPUT -p udp --sport 53 -j ACCEPT
+        iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+        iptables -A INPUT -p tcp --sport 53 -j ACCEPT
+            
+        iptables -A INPUT -s 147.52.65.68 -j ACCEPT
+        iptables -A OUTPUT -d 147.52.65.68 -j ACCEPT
+        iptables -A INPUT -s 147.52.67.72 -j ACCEPT
+        iptables -A OUTPUT -d 147.52.67.72 -j ACCEPT
+        ;;
 
-iptables -A INPUT -s 147.52.65.68 -j ACCEPT
-iptables -A OUTPUT -d 147.52.65.68 -j ACCEPT
-iptables -A INPUT -s 147.52.67.72 -j ACCEPT
-iptables -A OUTPUT -d 147.52.67.72 -j ACCEPT
+    off)
+        iptables -P INPUT ACCEPT
+        iptables -P OUTPUT ACCEPT
+        iptables -P FORWARD ACCEPT
 
-#iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-#iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+        iptables -F INPUT
+        iptables -F OUTPUT
+        iptables -F FORWARD
+        ;;
+    
+    status)
+        iptables-save
+        ;;
 
-#iptables -A OUTPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-#iptables -A INPUT  -p tcp --sport 22 -m state --state ESTABLISHED,RELATED -j ACCEPT
+    *)
+        echo $"Usage: $0 {on|off|status}"
+	RETVAL=1
+        ;;
+esac
 
+exit $RETVAL
