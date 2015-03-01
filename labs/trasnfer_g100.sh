96e2e22b6b6528fd2ad5ddef34909026de4dbf2b
Dimitris Kalopsikakis <kalopsik@turing>
2015-03-01 22:10:19 +0200

    20150301221019

--- labs/trasnfer_g100.sh
+++ labs/trasnfer_g100.sh
@@ -10,10 +10,10 @@ while (($I<=254)); do
 	echo executing on 147.52.58.$I: $COMMAND >>$I.out 2>&1;
         if [ $FLAG = 1 ];
         then
-	    scp -i ~/.ssh/id_rsa-labs-1502040018  -o "StrictHostKeyChecking no" $1 root@147.52.58.$I:/root/  >>$I.out 2>&1 & 
+	    scp -i ~/.ssh/id_rsa-labs-1502040018  -o "StrictHostKeyChecking no" $2 root@147.52.58.$I:/root/  >>$I.out 2>&1 & 
         elif [ $FLAG = 2 ];
         then
-	    scp -o "StrictHostKeyChecking no" $1 root@147.52.58.$I:/root/ >>$I.out 2>&1 & 
+	    scp -o "StrictHostKeyChecking no" $2 root@147.52.58.$I:/root/ >>$I.out 2>&1 & 
         else
             echo "nothing to do"
         fi
