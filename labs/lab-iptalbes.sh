d5354daac7eb86ae81e4f10212c66acbbfa9fd48
Dimitris Kalopsikakis <kalopsik@math.uoc.gr>
2015-03-02 09:27:20 +0200

    20150302092720

--- labs/lab-iptalbes.sh
+++ labs/lab-iptalbes.sh
@@ -19,10 +19,10 @@ case $1 in
         iptables -F OUTPUT
         iptables -F FORWARD
     
-        iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-        iptables -A INPUT -p udp --sport 53 -j ACCEPT
-        iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
-        iptables -A INPUT -p tcp --sport 53 -j ACCEPT
+        iptables -A OUTPUT -p udp --dport 53 -j DROP
+        iptables -A INPUT -p udp --sport 53 -j DROP
+        iptables -A OUTPUT -p tcp --dport 53 -j DROP
+        iptables -A INPUT -p tcp --sport 53 -j DROP
 
         iptables -A OUTPUT -p udp --dport 390 -j DROP
         iptables -A INPUT -p udp --sport 390 -j DROP
