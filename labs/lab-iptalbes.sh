167a0b25e5c2ffcdc2c14770834b451623653b8c
Dimitris Kalopsikakis <kalopsik@math.uoc.gr>
2015-03-02 13:57:38 +0200

    20150302135738

--- labs/lab-iptalbes.sh
+++ labs/lab-iptalbes.sh
@@ -8,33 +8,37 @@ case $1 in
 #        umount /home/math/ugrads
 #        umount /home/tem/ugrads
 
-#        iptables -P INPUT DROP
-#        iptables -P OUTPUT DROP
-#        iptables -P FORWARD DROP
-        iptables -P INPUT ACCEPT
-        iptables -P OUTPUT ACCEPT
-        iptables -P FORWARD ACCEPT
-        
+        iptables -P INPUT DROP
+        iptables -P OUTPUT DROP
+        iptables -P FORWARD DROP
+#        iptables -P INPUT ACCEPT
+#        iptables -P OUTPUT ACCEPT
+#        iptables -P FORWARD ACCEPT
+
+        iptables -A INPUT  -i lo -j ACCEPT
+        iptables -A OUTPUT -o lo -j ACCEPT
+
+
         iptables -F INPUT
         iptables -F OUTPUT
         iptables -F FORWARD
     
-        iptables -A OUTPUT -p udp --dport 53 -j DROP
-        iptables -A INPUT -p udp --sport 53 -j DROP
-        iptables -A OUTPUT -p tcp --dport 53 -j DROP
-        iptables -A INPUT -p tcp --sport 53 -j DROP
+        iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+        iptables -A INPUT -p udp --sport 53 -j ACCEPT
+        iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+        iptables -A INPUT -p tcp --sport 53 -j ACCEPT
 
-        iptables -A OUTPUT -p udp --dport 390 -j DROP
-        iptables -A INPUT -p udp --sport 390 -j DROP
-        iptables -A OUTPUT -p tcp --dport 390 -j DROP
-        iptables -A INPUT -p tcp --sport 390 -j DROP
+#        iptables -A OUTPUT -p udp --dport 390 -j DROP
+#        iptables -A INPUT -p udp --sport 390 -j DROP
+#        iptables -A OUTPUT -p tcp --dport 390 -j DROP
+#        iptables -A INPUT -p tcp --sport 390 -j DROP
             
-#        iptables -A INPUT -s 147.52.65.68 -j ACCEPT
-#        iptables -A OUTPUT -d 147.52.65.68 -j ACCEPT
-#        iptables -A INPUT -s 147.52.67.72 -j ACCEPT
-#        iptables -A OUTPUT -d 147.52.67.72 -j ACCEPT
-#        iptables -A INPUT -s 147.52.82.71 -j ACCEPT
-#        iptables -A OUTPUT -d 147.52.82.71 -j ACCEPT
+        iptables -A INPUT -s 147.52.65.68 -j ACCEPT
+        iptables -A OUTPUT -d 147.52.65.68 -j ACCEPT
+        iptables -A INPUT -s 147.52.67.72 -j ACCEPT
+        iptables -A OUTPUT -d 147.52.67.72 -j ACCEPT
+        iptables -A INPUT -s 147.52.82.71 -j ACCEPT
+        iptables -A OUTPUT -d 147.52.82.71 -j ACCEPT
         ;;
 
     off)
