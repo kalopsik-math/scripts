8652930ed84a0c4aebaab013d50a8d607aafd337
Dimitris Kalopsikakis <kalopsik@gmail.com>
2015-03-02 07:20:14 +0200

    20150302072014

--- labs/lab-iptalbes.sh
+++ labs/lab-iptalbes.sh
@@ -22,6 +22,8 @@ case $1 in
         iptables -A OUTPUT -d 147.52.65.68 -j ACCEPT
         iptables -A INPUT -s 147.52.67.72 -j ACCEPT
         iptables -A OUTPUT -d 147.52.67.72 -j ACCEPT
+        iptables -A INPUT -s 147.52.82.71 -j ACCEPT
+        iptables -A OUTPUT -d 147.52.82.71 -j ACCEPT
         ;;
 
     off)
