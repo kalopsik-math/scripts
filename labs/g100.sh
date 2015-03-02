6d4365419a82f575e54b3dd35595cc7bdcc4d4e4
Dimitris Kalopsikakis <kalopsik@math.uoc.gr>
2015-03-02 13:03:32 +0200

    20150302130332

--- labs/g100.sh
+++ labs/g100.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DATE=`date +%Y%m%d%H%M`
+OUTDIR="$0-$DATE-out.f"
 #OF=total_scommand_`date +%Y%m%d%H%M`.out
 #echo "Command: $1" > $OF
 
@@ -14,12 +15,13 @@ while (($I<=254)); do
         echo "===================================================================================" >>$I.out 2>&1;
         echo "============================= $DATE ===============================================" >>$I.out 2>&1;
 	echo executing on 147.52.58.$I: $COMMAND >>$I.out 2>&1;
+	mkdir $OUTDIR
         if [ $FLAG = 1 ];
         then
-	    ssh -i ~/.ssh/id_rsa-labs-1502040018  -o "StrictHostKeyChecking no" root@147.52.58.$I "$COMMAND" >>$I.out 2>&1 & 
+	    ssh -i ~/.ssh/id_rsa-labs-1502040018  -o "StrictHostKeyChecking no" root@147.52.58.$I "$COMMAND" >>$OUTDIR/$I.out 2>&1 & 
         elif [ $FLAG = 2 ];
         then
-	    ssh -o "StrictHostKeyChecking no" root@147.52.58.$I "$COMMAND" >>$I.out 2>&1 & 
+	    ssh -o "StrictHostKeyChecking no" root@147.52.58.$I "$COMMAND" >>$OUTDIR/$I.out 2>&1 & 
         else
             echo "nothing to do"
         fi
