7332a3ace0b5f722b12fb5e2c75cf5060d0d2ffe
Dimitris Kalopsikakis <kalopsik@gmail.com>
2015-03-02 09:24:59 +0200

    20150302092459

--- labs/lab-iptalbes.sh
+++ labs/lab-iptalbes.sh
@@ -5,12 +5,15 @@ RETVAL=1
 case $1 in
 
     on)
-        umount /home/math/ugrads
-        umount /home/tem/ugrads
+#        umount /home/math/ugrads
+#        umount /home/tem/ugrads
 
-        iptables -P INPUT DROP
-        iptables -P OUTPUT DROP
-        iptables -P FORWARD DROP
+#        iptables -P INPUT DROP
+#        iptables -P OUTPUT DROP
+#        iptables -P FORWARD DROP
+        iptables -P INPUT ACCEPT
+        iptables -P OUTPUT ACCEPT
+        iptables -P FORWARD ACCEPT
         
         iptables -F INPUT
         iptables -F OUTPUT
@@ -20,13 +23,18 @@ case $1 in
         iptables -A INPUT -p udp --sport 53 -j ACCEPT
         iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
         iptables -A INPUT -p tcp --sport 53 -j ACCEPT
+
+        iptables -A OUTPUT -p udp --dport 390 -j DROP
+        iptables -A INPUT -p udp --sport 390 -j DROP
+        iptables -A OUTPUT -p tcp --dport 390 -j DROP
+        iptables -A INPUT -p tcp --sport 390 -j DROP
             
-        iptables -A INPUT -s 147.52.65.68 -j ACCEPT
-        iptables -A OUTPUT -d 147.52.65.68 -j ACCEPT
-        iptables -A INPUT -s 147.52.67.72 -j ACCEPT
-        iptables -A OUTPUT -d 147.52.67.72 -j ACCEPT
-        iptables -A INPUT -s 147.52.82.71 -j ACCEPT
-        iptables -A OUTPUT -d 147.52.82.71 -j ACCEPT
+#        iptables -A INPUT -s 147.52.65.68 -j ACCEPT
+#        iptables -A OUTPUT -d 147.52.65.68 -j ACCEPT
+#        iptables -A INPUT -s 147.52.67.72 -j ACCEPT
+#        iptables -A OUTPUT -d 147.52.67.72 -j ACCEPT
+#        iptables -A INPUT -s 147.52.82.71 -j ACCEPT
+#        iptables -A OUTPUT -d 147.52.82.71 -j ACCEPT
         ;;
 
     off)
@@ -38,7 +46,7 @@ case $1 in
         iptables -F OUTPUT
         iptables -F FORWARD
 
-        mount -a
+#        mount -a
         ;;
     
     status)
