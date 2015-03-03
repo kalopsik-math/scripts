53d341b75b8038f1d843ca306ff018535d506372
kalopsik <kalopsik@math.uoc.gr>
2015-03-03 14:36:19 +0200

    Added exception. Sends email to sysadmin@math.uoc.gr when exception is raised.

--- labs/check_idle.py
+++ labs/check_idle.py
@@ -13,49 +13,65 @@
 #       my_notifier.py must be in /usr/local/bin
 
 import sys
-import subprocess
 
-# Maximum minutes of no X user activity
-timeout  = 30
+try:
+    import subprocess
 
-# Seconds before shutdown
-margin   = 300
+    # Maximum minutes of no X user activity (30)
+    timeout  = 30
 
-# Program to run when timeout is reached
-locker   = "/sbin/poweroff"
+    # Seconds before shutdown (300)
+    margin   = 300
 
-# Program to run $margin seconds before timeout
-# notifier = "/usr/local/bin/ny_notifier.py"
-notifier = "/home/kalopsik/PycharmProjects/scripts/labs/my_notifier.py"
+    # Program to run when timeout is reached
+    locker   = "/sbin/poweroff"
 
-# #notifier = "/usr/bin/notify-send"
-# summary  = "\"Ο υπολογιστής κλείνει σε 5min\""
-# message  = "\"Ο υπολογιστής είναι ανενεργός για μισή ώρα· αν δεν κουνήσετε το ποντίκι ή αν δε \
-# πατήσετε κάποιο πλήκτρο, θα κλείσει σε 5 λεπτά.\""
+    # Program to run $margin seconds before timeout
+    # notifier = "/usr/local/bin/ny_notifier.py"
+    notifier = "/home/kalopsik/PycharmProjects/scripts/labs/my_notifier.py"
 
-# xautolock executable
-xautolock = "/usr/bin/xautolock"
+    logfile = "/var/log/check_idle.log"
+    log = open(logfile,"a")
 
-opts = []
-opts.append("-time")
-opts.append("%d" %timeout)
-opts.append("-notify")
-opts.append("%d" % margin)
-opts.append("-notifier")
-opts.append("%s " % notifier)
-opts.append("-locker")
-opts.append("%s" % locker)
-opts.append("-secure")
-opts.append("-detectsleep")
+    # xautolock executable
+    xautolock = "/usr/bin/xautolock"
 
-commandlist = []
-commandlist.append(xautolock)
+    opts = []
+    opts.append("-time")
+    opts.append("%d" %timeout)
+    opts.append("-notify")
+    opts.append("%d" % margin)
+    opts.append("-notifier")
+    opts.append("%s " % notifier)
+    opts.append("-locker")
+    opts.append("%s" % locker)
+    opts.append("-secure")
+    opts.append("-detectsleep")
 
-print(commandlist+opts)
-popen = subprocess.Popen(commandlist+opts,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
+    commandlist = []
+    commandlist.append(xautolock)
 
-stdout,stderr = popen.communicate()
+    log.write("Executing: %s" % "",join(commandlist+opts))
+    popen = subprocess.Popen(commandlist+opts,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
 
-print(stdout,stderr)
+    stdout,stderr = popen.communicate()
+
+    log.write("%s||%s" % (stdout,stderr))
+    log.close()
+    raise()
+except:
+
+    import smtplib
+    from email.mime.text import MIMEText
+
+    message = "ERROR: %s" % sys.exc_info()[0]
+    msg = MIMEText(message)
+    msg['Subject'] = '[check_idle] %s' % message[:20]
+    msg['From'] = "check_idle@math.uoc.gr"
+    msg['To'] = "sysadmin@math.uoc.gr"
+
+    s = smtplib.SMTP('mta.mail.uoc.gr')
+    s.sendmail("sysadmin@math.uoc.gr", "sysadmin@math.uoc.gr", msg.as_string())
+    s.quit()
