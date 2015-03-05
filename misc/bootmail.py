567ed808da8afaaa0c89b5726c94a4d23b4260a6
kalopsik <kalopsik@math.uoc.gr>
2015-03-05 09:53:07 +0200

    Added  ip address and the correct email addresses

--- misc/bootmail.py
+++ misc/bootmail.py
@@ -31,23 +31,24 @@ popen = subprocess.Popen(command.split(),
 stag = popen.communicate()[0].strip()
 print("command name %s " % sys.argv[0])
 ss = ""
-if sys.argv[1].strip() == "start":
+if len(sys.argv) >= 2 and sys.argv[1].strip() == "start":
     ss = "Booting up"
-elif sys.argv[1].strip() == "stop":
+elif len(sys.argv) >= 2 and sys.argv[1].strip() == "stop":
     ss = "Shutting down"
 else :
     ss = "Oops. Nor starting neither stopping..."
 
 t = time.strftime("%Y%m%d%H%M%S").rstrip('\n')
-message = t + ":: " + ss + " computer with mac address %s and service tag: %s " % (hex(mac),stag)
+message = t + ":: " + ss + " computer with ip %s, mac address %s and service tag: %s " % \
+                           (ip_address, hex(mac), stag)
 
 msg = MIMEText(message)
 msg['Subject'] = '[g100-boot] %s, %s' % (ss,stag)
 msg['From'] = "g100-boot@math.uoc.gr"
 msg['To'] = "sysadmin@math.uoc.gr"
-msg['Cc'] = "kalopsik@math.uoc.gr"
-msg['BCc'] = "kalopsik@gmail.com"
-to = [msg['To'], msg['Cc'], msg['BCc']]
+msg['Cc'] = "kalopsik@math.uoc.gr kolount@gmail.com"
+#msg['BCc'] = "kalopsik@gmail.com"
+to = [msg['To']] + msg['Cc'].split()
 fr = msg['From']
 s = smtplib.SMTP('mta.uoc.gr')
 s.sendmail(fr, to, msg.as_string())
