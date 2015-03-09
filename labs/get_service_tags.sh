1fc1e1625ea4d4487f3d7ef1011b4a22d46fe6aa
kalopsik <kalopsik@math.uoc.gr>
2015-03-09 13:34:03 +0200

    20150309133403

--- labs/get_service_tags.sh
+++ labs/get_service_tags.sh
@@ -3,14 +3,14 @@
 DATE=`date +%Y%m%d%H%M%S`
 OUTDIR="g100-$DATE-out.d"
 OUTFILE="service_tags"
-COMMAND="dmidecode -s system-serial-number"
 FLAG=$1
 
 
 ((I=4)); 
 while (($I<=254)); do 
+    IP="147.52.58.$I"
+    COMMAND="echo $IP && dmidecode -s system-serial-number"
     if [ ! -d $OUTDIR ]; then mkdir $OUTDIR; fi
-    echo "147.52.58.$I" >> $OUTDIR/$OUTFILE
     if [ $FLAG = 1 ];
     then
 	    ssh -i ~/.ssh/id_rsa-labs-1502040018  -o "StrictHostKeyChecking no" root@147.52.58.$I "$COMMAND" >>$OUTDIR/$OUTFILE &
