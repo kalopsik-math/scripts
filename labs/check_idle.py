03eb5a2374f5a37a76c1d6ef551a19166398e941
kalopsik <kalopsik@math.uoc.gr>
2015-03-31 13:09:04 +0300

    Added  ip address and the correct email addresses

--- labs/check_idle.py
+++ labs/check_idle.py
@@ -12,7 +12,10 @@
 #       xautolock must be installe
 #       my_notifier.py must be in /usr/local/bin
 
-
+import sys
+import time
+import os
+currenttime = time.strftime("%Y%m%d%H%M%S").rstrip('\n')
 try:
     import subprocess
 
@@ -35,6 +38,10 @@ try:
     logfile = "/var/log/check_idle.log"
     log = open(logfile,"a")
 
+    my_env = os.environ
+    my_env['DISPLAY'] = ":0.0"
+    my_env['XAUTHORITY'] = "/root/.Xauthoroty"
+
     # xautolock executable
     xautolock = "/usr/bin/xautolock"
 
@@ -42,14 +49,13 @@ try:
     opts.append("-time")
     opts.append("%d" %timeout)
 
-    import os
     if os.path.isfile(notifier):
         opts.append("-notify")
         opts.append("%d" % margin)
         opts.append("-notifier")
         opts.append("%s " % notifier)
     else:
-        log.write("Warning! Notifier (/usr/local/bin/my_notifier.py) not found.\n")
+        log.write("%s: Warning! Notifier (/usr/local/bin/my_notifier.py) not found.\n" % currenttime)
     opts.append("-locker")
     opts.append("%s" % locker)
     opts.append("-secure")
@@ -58,29 +64,34 @@ try:
     commandlist = []
     commandlist.append(xautolock)
 
-    log.write("Executing: %s\n" % " ".join(commandlist+opts))
+    log.write("%s: Executing: %s\n" % (currenttime, " ".join(commandlist+opts)))
     popen = subprocess.Popen(commandlist+opts,
                              stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
+                             stderr=subprocess.PIPE,
+                             env=my_env)
 
     stdout,stderr = popen.communicate()
 
-    log.write("%s || %s\n" % (stdout,stderr))
+    log.write("%s: %s || %s\n" % (currenttime, stdout, stderr))
     log.close()
-    raise()
+    #raise()
 
 except:
 
-    import sys
-    import smtplib
-    from email.mime.text import MIMEText
-
-    message = "ERROR: %s" % sys.exc_info()[0]
-    msg = MIMEText(message)
-    msg['Subject'] = '[check_idle] %s' % message[:20]
-    msg['From'] = "check_idle@math.uoc.gr"
-    msg['To'] = "sysadmin@math.uoc.gr"
-
-    s = smtplib.SMTP('mta.uoc.gr')
-    s.sendmail("sysadmin@math.uoc.gr", "sysadmin@math.uoc.gr", msg.as_string())
-    s.quit()
+    logfile = "/var/log/check_idle.log"
+    log = open(logfile,"a")
+    log.write("%s: There was an error %s " % (currenttime, sys.exc_info()[0]))
+    log.close()
+    # import sys
+    # import smtplib
+    # from email.mime.text import MIMEText
+    #
+    # message = "ERROR: %s" % sys.exc_info()[0]
+    # msg = MIMEText(message)
+    # msg['Subject'] = '[check_idle] %s' % message[:20]
+    # msg['From'] = "check_idle@math.uoc.gr"
+    # msg['To'] = "sysadmin@math.uoc.gr"
+    #
+    # s = smtplib.SMTP('mta.uoc.gr')
+    # s.sendmail("sysadmin@math.uoc.gr", "sysadmin@math.uoc.gr", msg.as_string())
+    #s.quit()
